a69f4f263ef02207adfe8e55ef50dda0bf60e27b
Mark Roberts <mark@markroberts.org.uk>
2023-04-02 08:34:05 +0100

    limit outputs

--- src/lib/risco-mqtt-local.ts
+++ src/lib/risco-mqtt-local.ts
@@ -410,7 +410,7 @@ export function riscoMqttHomeAssistant(userConfig: RiscoMQTTConfig) {
     return outputs.values.filter(o => o.UserUsable !== false && o.Pulsed);
   }
   function activeSystemOutputs(systemoutputs: OutputList): Output[] {
-    return systemoutputs.values.filter(o => o.UserUsable === false);
+    return systemoutputs.values.filter(o => o.UserUsable === false && o.Label !=='');
   }
 
   function publishOnline() {
