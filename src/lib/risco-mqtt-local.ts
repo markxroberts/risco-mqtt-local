a332d8ebab4ff5e9d69d1fd969bdf6e951ea8277
Mark Roberts <mark@markroberts.org.uk>
2023-04-07 06:31:45 +0100

    Update package.json

--- src/lib/risco-mqtt-local.ts
+++ src/lib/risco-mqtt-local.ts
@@ -377,7 +377,7 @@ export function riscoMqttHomeAssistant(userConfig: RiscoMQTTConfig) {
     mqttClient.publish(`${config.risco_node_id}/alarm/output/${output.Id}/status`, outputState(output), {
       qos: 1, retain: false,
     });
-    logger.verbose(`[Panel => MQTT] Published output status ${output.OStatus} on output ${output.Label}`);
+    logger.verbose(`[Panel => MQTT] Published output status ${output.Status} on output ${output.Label}`);
   }
 
   function publishZoneBypassStateChange(zone: Zone) {
