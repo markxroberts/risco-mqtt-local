75ea795df635d5d6caa7d11d4a29c05f8650ec4e
Mark Roberts <mark@markroberts.org.uk>
2023-07-20 20:28:46 +0100

    More debugging

--- src/lib/risco-mqtt-local.ts
+++ src/lib/risco-mqtt-local.ts
@@ -406,12 +406,15 @@ export function riscoMqttHomeAssistant(userConfig: RiscoMQTTConfig) {
       const riscoState = returnRiscoAlarmState(partition);
       logger.debug(`Risco Panel alarm state is ${riscoState}.`);
       let partitionAlarmMapping: ArmingConfig
+      logger.debug(`Risco mapping to arm_away is ${alarmMapping[partitionLabel].arm_away}.`)
+      const arm_away = alarmMapping[partitionLabel].arm_away
+      logger.debug(`Value of arm_away is ${arm_away}.`)
       partitionAlarmMapping = {
-        arm_away: alarmMapping[partitionLabel][arm_away],
-        arm_home: alarmMapping[partitionLabel][arm_home],
-        arm_night: alarmMapping[partitionLabel][arm_night],
-        arm_vacation: alarmMapping[partitionLabel][arm_vacation],
-        arm_custom_bypass: alarmMapping[partitionLabel][arm_custom_bypass]
+        arm_away: alarmMapping[partitionLabel].arm_away,
+        arm_home: alarmMapping[partitionLabel].arm_home,
+        arm_night: alarmMapping[partitionLabel].arm_night,
+        arm_vacation: alarmMapping[partitionLabel].arm_vacation,
+        arm_custom_bypass: alarmMapping[partitionLabel].arm_custom_bypass
       }
       logger.verbose(`Partition mapping for ${partitionLabel} is ${partitionAlarmMapping}.`);
       const alarmKey = Object.keys(partitionAlarmMapping as (keyof typeof partitionAlarmMapping)[]).find((key) => {
