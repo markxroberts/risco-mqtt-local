03a113e0872edc90b3e701cb84f9b82b575e3159
Mark Roberts <mark@markroberts.org.uk>
2023-07-20 19:21:57 +0100

    Alternate alarm state mapping

--- src/lib/risco-mqtt-local.ts
+++ src/lib/risco-mqtt-local.ts
@@ -402,11 +402,17 @@ export function riscoMqttHomeAssistant(userConfig: RiscoMQTTConfig) {
     } else {
       const riscoState = returnRiscoAlarmState(partition);
       logger.debug(`Risco Panel alarm state is ${riscoState}.`);
-      const payloadMapping = Object.values(alarmMapping[partition.Label]).indexOf(riscoState as unknown as typeof alarmMapping);
-      logger.debug(`Mapping of alarm state is ${payloadMapping}.`)
-      const alarmKey = Object.keys(alarmMapping[partition.Label])[payloadMapping];
-      logger.debug(`HA version of alarm state is ${alarmKey}.`)
+      const partitionAlarmMapping = {
+        arm_away: alarmMapping[partitionLabel].arm_away,
+        arm_home: alarmMapping[partitionLabel].arm_home,
+        arm_night: alarmMapping[partitionLabel].arm_night,
+        arm_vacation: alarmMapping[partitionLabel].arm_vacation,
+        arm_custom_bypass: alarmMapping[partitionLabel].arm_custom_bypass
+      };
+      const alarmKey = Object.keys(partitionAlarmMapping as (keyof typeof partitionAlarmMapping)[]).find((key) => {
+        return partitionAlarmMapping[key] === riscoState});
       return alarmKey
+      logger.debug(`HA version of alarm state is ${alarmKey}.`);
     }
   }
   function outputState(output: Output, EventStr: string) {
@@ -684,7 +690,7 @@ export function riscoMqttHomeAssistant(userConfig: RiscoMQTTConfig) {
         }};
       alarmMapping.push(alarmRemap);
       logger.info(`Added alarm state mapping for partition ${partitionLabel}.`)
-      logger.debug(`Added alarm state mappings for parition ${partitionLabel} as ${alarmRemap}.`)
+      logger.verbose(`Added alarm state mappings for parition ${partitionLabel} as ${alarmRemap}.`)
       
       const payload = {
         name: partition.Label,
