ec54b7c596a169c3bf5fba1cb1678ad0c6fb890a
Mark Roberts <mark@markroberts.org.uk>
2025-10-11 20:20:46 +0100

    YAML config

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@
 const fs = require('fs');
 const path = require('path');
 import {riscoMqttHomeAssistant} from './lib';
+import { RiscoMQTTConfig } from './index';
+import yaml from 'js-yaml';
 
 try {
     let configPath = ""
+    let json = true
     if ("RISCO_MQTT_HA_CONFIG_FILE" in process.env) {
         // if this var is set, we know we are running in the addon
         configPath = process.env.RISCO_MQTT_HA_CONFIG_FILE
@@ -14,18 +17,38 @@ try {
         if (!fs.existsSync(configPath) && fs.existsSync(sampleConfigPath)) {
             fs.copyFileSync(sampleConfigPath, configPath);
         }
+    } else if ("RISCO_MQTT_HA_CONFIG_YAML" in process.env) {
+        // if this var is set, we know we are running in the addon
+        configPath = process.env.RISCO_MQTT_HA_CONFIG_YAML
+        // check if is file
+        const sampleConfigPath = path.join(__dirname, "../config-sample.yaml")
+        if (!fs.existsSync(configPath) && fs.existsSync(sampleConfigPath)) {
+            fs.copyFileSync(sampleConfigPath, configPath);
+            json = false;
+        }
     } else {
-        configPath = path.join(process.cwd(), 'config.json')
+        configJSON = path.join(process.cwd(), 'config.json')
+        configYAML = path.join(process.cwd(), 'config.yaml')
+        if (fs.existsSync(configJSON)) {
+            configPath = configJSON
+        } else {
+            configPath = configYAML
+            json = false
+        }
     }
-    console.log('Loading config from: ' + configPath)
-    if (fs.existsSync(configPath)) {
+    console.log('[RML] Loading config from: ' + configPath)
+    if (fs.existsSync(configPath) && json) {
         const config = require(configPath)
         riscoMqttHomeAssistant(config)
+    }
+    else if (fs.existsSync(configPath) && !json) {
+        const config = yaml.load(readFileSync(configPath, 'utf-8') as RiscoMQTTConfig)
+        riscoMqttHomeAssistant(config)
     } else {
-        console.log(`file ${configPath} does not exist`)
+        console.log(`[RML] File ${configPath} does not exist`)
         process.exit(1)
     }
 } catch (e) {
-    console.error('Startup error', e)
+    console.error('[RML] Startup error', e)
     process.exit(1)
 }
